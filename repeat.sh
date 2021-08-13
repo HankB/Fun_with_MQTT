6f663a3a59300666031c7d3279c804b092e54748
Hank Barta <hbarta@gmail.com>
2021-08-12 22:43:04 -0500

    add set -e

--- repeat.sh
+++ repeat.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+set -e # exit on error
+
 while(:); do echo "I'm still here"; sleep 5; done \
 |mosquitto_pub -t TEST -h localhost  --will-payload "I'm now gone" --will-topic "test/will" -l

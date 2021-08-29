60ecdf5c813e727e4171ed4e15c3320cb1766cea
Hank Barta <hbarta@gmail.com>
2021-08-28 22:14:02 -0500

    repeat.sh

--- repeat.sh
+++ repeat.sh
@@ -2,15 +2,18 @@
 
 set -e # exit on error
 
-# connect and send periodic message to broker
+# connect and send startup message followed by periodic messages
 
 host=$(hostname)
 
-while(:)
-do
-    echo "$host still here"
-    sleep 5
-done | \
+(
+    echo "$host connecting"; \
+    while(:)
+    do
+        sleep 5
+        echo "$host still here"
+    done 
+) | \
 mosquitto_pub  -t TEST \
                -h localhost \
                --will-payload "$host now gone" \
